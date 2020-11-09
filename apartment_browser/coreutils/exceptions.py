89a9710150e29f1106411f400776c5145b647c73
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2020-11-09 23:26:10 +0100

    fix some errors

--- apartment_browser/coreutils/exceptions.py
+++ apartment_browser/coreutils/exceptions.py
@@ -1,12 +1,30 @@
+"""
+Declare custom exception used in apartment_browser package.
+"""
 import sanic
-import json
 
-class APIError(Exception):
+
+class ApartmentBrowserException(Exception):
+    """
+    Base exception for all exceptions defined here.
+    """
+    pass
+
+
+class APIError(ApartmentBrowserException):
+    """
+    Special exception that can be serialized in API.
+    """
+
     def __init__(self, message, status=500, **info):
+        super().__init__(message)
         self._json = dict(message=message, **info)
         self._status = status
 
     def to_response(self):
+        """
+        Convert this exception into sanic response to be returned by API.
+        """
         return sanic.response.json(
             self._json, status=self._status
         )
--- apartment_browser/coreutils/service.py
+++ apartment_browser/coreutils/service.py
@@ -4,6 +4,10 @@ from .exceptions import APIError
 
 
 class BaseService:
+    """
+    Abstract class used to simplify interactions with mongodb collections.
+    """
+
     @classmethod
     def object_id(cls, uid):
         """
@@ -16,12 +20,17 @@ class BaseService:
 
     @classmethod
     def search(cls, collection, filters={}):
+        """
+        Return a list of objects matching given filters.
+        """
         apartments = [apartment for apartment in collection.find(filters)]
         return apartments
 
     @classmethod
     def get_details(cls, collection, uid):
-        print(collection.name)
+        """
+        Return item of the collection with given uid
+        """
         result = collection.find_one({'_id': cls.object_id(uid)})
         if result is None:
             raise APIError(
@@ -30,32 +39,47 @@ class BaseService:
 
     @classmethod
     def validate(cls, data, schema=None):
+        """
+        Validate a dictionnary against given schema or raise 404.
+        """
         if data is None:
-            raise APIError("Data of the apartment to be created must be passed a json body", status=400)
-        
+            raise APIError(
+                "Data of the apartment to be created must be passed a json body", status=400)
+
         try:
             if schema is None:
                 return data
             else:
                 return schema.load(data)
         except ValidationError as error:
-            raise APIError("Error validating input data", causes=error.messages)
+            raise APIError("Error validating input data",
+                           causes=error.messages)
 
     @classmethod
     def create(cls, collection, data, schema=None):
+        """
+        Create object if it passes schema validation.
+        """
         validated = cls.validate(data, schema=schema)
         result = collection.insert_one(validated)
         return collection.find_one({'_id': result.inserted_id})
 
     @classmethod
     def update(cls, collection, uid, data):
+        """
+        If data is a valid object, replace existing object with new one.
+        """
         validated = cls.validate(data)
-        validated.pop('_id', None) # remove field "_id" if set
+        validated.pop('_id', None)  # remove field "_id" if set
         object_uid = cls.object_id(uid)
-        result = collection.update_one({'_id': object_uid}, {"$set": validated}, upsert=True)
+        collection.update_one({'_id': object_uid}, {
+                              "$set": validated}, upsert=True)
         return collection.find_one({'_id': object_uid})
 
     @classmethod
     def delete(cls, collection, uid):
+        """
+        Delete an object identified by its uid.
+        """
         result = collection.remove({'_id': cls.object_id(uid)})
         return result
