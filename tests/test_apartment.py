cd0217789059d37a50acfc9bc717a8cd42ad927f
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2020-11-10 14:05:08 +0100

    configure code coverage

--- tests/test_apartment.py
+++ tests/test_apartment.py
@@ -1,3 +1,10 @@
+from apartment_browser.coreutils.service import BaseService
 
-def test_apartment_browser_import():
-    import apartment_browser
+
+def test_base_service_interface():
+    assert hasattr(BaseService, "search")
+    assert hasattr(BaseService, "get_details")
+    assert hasattr(BaseService, "validate")
+    assert hasattr(BaseService, "create")
+    assert hasattr(BaseService, "update")
+    assert hasattr(BaseService, "delete")
